43103dde82a626c97b478e24114a8f950c0b4422
Xin Liang <lx.altria@gmail.com>
2022-07-31 21:23:05 -0500

    align zstd allocation

--- include/LosslessCompressor/ZSTD.hpp
+++ include/LosslessCompressor/ZSTD.hpp
@@ -7,13 +7,13 @@ namespace MDR {
     namespace ZSTD{
         #define ZSTD_LEVEL 3 //default setting of level is 3
         // ZSTD lossless compressor
-        uint32_t compress(uint8_t* data, uint32_t dataLength, uint8_t** compressBytes) {
+        uint32_t compress(const uint8_t* data, uint32_t dataLength, uint8_t** compressBytes) {
             uint32_t outSize = 0; 
             size_t estimatedCompressedSize = 0;
-            if(dataLength < 100) 
-                estimatedCompressedSize = 200;
+            if(dataLength < 1024) 
+                estimatedCompressedSize = 2048;
             else
-                estimatedCompressedSize = dataLength*1.2;
+                estimatedCompressedSize = (dataLength*1.2)/8 * 8;
             *compressBytes = (uint8_t*)malloc(estimatedCompressedSize);
             *reinterpret_cast<size_t*>(*compressBytes) = dataLength;
             outSize = ZSTD_compress(*compressBytes + sizeof(size_t), estimatedCompressedSize, data, dataLength, ZSTD_LEVEL); 
